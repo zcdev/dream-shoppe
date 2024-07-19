85bf037e99403c243cf1b1e1a2afe7fea02a6672
zcdev <zcdev@MyMacBookPro.local>
2024-07-18 19:18:47 -0700

    Center elements

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ const Shop = () => {
   ));
 
   return (
-    <main className="bg-[#f8f7f5] min-h-[calc(100vh-76px)] px-10 py-8">
+    <main className="bg-[#f8f7f5] min-h-[calc(100vh-76px)] px-10 py-8 flex flex-col items-center">
       <button className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={handleGenerate}>Generate your points</button>
       <div className="pb-5">Here are your points to shop: {points}</div>
       <div className="right-0 top-0 md:right-20 md:mt-5 fixed bg-white p-4 md:p-8 md:rounded-xl shadow-md w-full md:w-max z-10">
